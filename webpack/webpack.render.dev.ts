6fce9128acadcec1d9d8d964ce15798cbc3fdff6
liaokongVFX <568250549@qq.com>
2021-12-11 20:12:02 +0800

    完成环境配置

---
+++ webpack/webpack.render.dev.ts
@@ -0,0 +1,60 @@
+const path = require('path');
+const webpackMerge = require('webpack-merge');
+const baseConfig = require('./webpack.base.js');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const devConfig = {
+    mode: 'development',
+    entry: {
+        // 对应渲染进程的 app.tsx 入口文件
+        index: path.resolve(__dirname, '../app/renderer/app.tsx'),
+    },
+    output: {
+        filename: '[name].[hash].js',
+        path: path.resolve(__dirname, '../dist'),
+    },
+    target: 'electron-renderer',
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: path.join(__dirname, '../dist'),
+        compress: true,
+        host: '127.0.0.1', // webpack-dev-server启动时要指定ip，不能直接通过localhost启动，不指定会报错
+        port: 7001, // 启动端口为 7001 的服务
+        hot: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            // 以此文件为模版，自动生成 HTML
+            template: path.resolve(__dirname, '../app/renderer/index.html'),
+            filename: path.resolve(__dirname, '../dist/index.html'),
+            chunks: ['index'],
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader', 'postcss-loader'],
+            },
+            {
+                test: /\.less$/,
+                exclude: /node_modules/,
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: {
+                                localIdentName: '[name]__[local]__[hash:base64:5]',
+                            },
+                        },
+                    },
+                    'postcss-loader',
+                    'less-loader',
+                ],
+            },
+        ],
+    }
+};
+
+module.exports = webpackMerge.merge(baseConfig, devConfig);
